f5bd63f3c5741b36066b92168ab3a50b0f5c02ae
tanmaysingh108 <tanmaysingh1080@gmail.com>
2026-08-29 00:23:07 +0530

    Add Two-Sum (hash-table)

--- leetcode/hash-table/1-Two-Sum.py
+++ leetcode/hash-table/1-Two-Sum.py
@@ -3,5 +3,3 @@ class Solution(object):
         for i in range(len(nums)):
             if target-nums[i] in nums and nums.index(target-nums[i])!=i:
                 return([i,nums.index(target-nums[i])])
-            else:
-                continue
