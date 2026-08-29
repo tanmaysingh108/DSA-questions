d60c53eb107e736ccad44639150058daf20ae45b
tanmaysingh108 <tanmaysingh1080@gmail.com>
2026-08-29 15:56:18 +0530

    Add Chef and Chocolates (400)

---
+++ codechef/400/CHEFCHOCO - Chef and Chocolates.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	int c,x,y;
+	for (T;T>0;T--){
+	    cin >> c;
+	    cin >> x;
+	    cin >> y;
+	    cout << (c-x)*y << "\n";
+	}
+
+}
