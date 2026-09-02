4b6d75d72a0b4f9456e75406363b10aceb8ff1b3
tanmaysingh108 <tanmaysingh1080@gmail.com>
2026-09-02 15:43:03 +0530

    Add Janmansh and Assignments (500)

---
+++ codechef/500/JASSIGNMENTS - Janmansh and Assignments.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	int x;
+	for (;T>0;T--){
+	    cin >> x;
+	    if (10-x>=3)
+	        cout << "Yes\n";
+	    else 
+	        cout << "No\n";
+	}
+}
