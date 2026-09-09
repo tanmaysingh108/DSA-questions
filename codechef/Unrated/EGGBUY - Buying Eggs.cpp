370d5d2e4e5cb59c2c2b8e7a841ba300430904c7
tanmaysingh108 <tanmaysingh1080@gmail.com>
2026-09-09 22:49:55 +0530

    Add Buying Eggs (Unrated)

---
+++ codechef/Unrated/EGGBUY - Buying Eggs.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x,y,f;
+	cin>>x;
+	cin>>y;
+	cin>>f;
+	if (12*x >= 12*y+f){
+	    cout<<(12*y+f);
+	}
+	else{
+	    cout<<(12*x);
+	}
+	}
+
+
