646b345360a1441f4fbee2b7f3a7b1f5785fd9d9
tanmaysingh108 <tanmaysingh1080@gmail.com>
2026-09-10 22:02:31 +0530

    Add Hotel Bytelandia (1400)

--- codechef/1400/HOTEL - Hotel Bytelandia.cpp
+++ codechef/1400/HOTEL - Hotel Bytelandia.cpp
@@ -2,42 +2,34 @@
 using namespace std;
 
 int main() {
-    int T;
-    cin >> T;
+	int t;
+	cin>>t;
+	for (;t>0;t--){
+	    int n;
+	    cin>>n;
+	    int A[n];
+	    int B[n];
+	    for (int i=0;i<n;i++){
+	        cin>>A[i];
+	    }
+	    for (int i=0;i<n;i++){
+	        cin>>B[i];
+	    }
+	    sort(A,A+n);
+	    sort(B,B+n);
+	    int max=0;
+	    for (int i=0,c=1;i<n;i++,c++){
+	        for (int j=0;j<n;j++){
+	            if (A[i]>=B[j]){
+	                B[j]=1001;
+	                c--;
+	            }
+	        }
+	        if (c>=max){
+	            max=c;
+	        }
+	    }
+	    cout<<max<<"\n";
+	}
 
-    while (T--) {
-        int n;
-        cin >> n;
-
-        int A[n], B[n];
-
-        for (int i = 0; i < n; i++)
-            cin >> A[i];
-
-        for (int i = 0; i < n; i++)
-            cin >> B[i];
-
-        sort(A, A + n);
-        sort(B, B + n);
-
-        int i = 0, j = 0;
-        int current = 0;
-        int ans = 0;
-
-        while (i < n && j < n) {
-            if (A[i] < B[j]) {
-                current++;
-                ans = max(ans, current);
-                i++;
-            }
-            else {
-                current--;
-                j++;
-            }
-        }
-
-        cout << ans << '\n';
-    }
-
-    return 0;
 }
