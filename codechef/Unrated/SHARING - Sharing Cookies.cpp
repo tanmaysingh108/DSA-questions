898d744d373a3957d2b03015a1b2f4f8b9eaa59b
tanmaysingh108 <tanmaysingh1080@gmail.com>
2026-09-16 20:16:27 +0530

    Add Sharing Cookies (Unrated)

---
+++ codechef/Unrated/SHARING - Sharing Cookies.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a;
+    cin>>b;
+    if ((a-b)%2==0){
+       int x=(a-b)/2;
+       cout<<(x)<<"\n";
+}   else{
+        cout<<(-1)<<"\n";
+}
+}
