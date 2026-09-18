a0e3f05497e04314cbd2adcf67ca8398c3743679
tanmaysingh108 <tanmaysingh1080@gmail.com>
2026-09-19 01:47:11 +0530

    Add Floyd's triangle (Basic)

--- geeksforgeeks/Basic/Floyd's triangle.py
+++ geeksforgeeks/Basic/Floyd's triangle.py
@@ -3,15 +3,17 @@
 int main() {
     int n;
     scanf("%d",&n);
-    
+
     for(int i=1,j=1;i<n+1;i++){
-        
-        for (int c=j;j<i+c;j++){
-            
-            printf("%d ",j);
+
+        for (int c=0;c<i;c++){
+
+            printf("%d ",j++);
+
         }
+
         printf("\n");
     }
-    
+
     return 0;
 }
