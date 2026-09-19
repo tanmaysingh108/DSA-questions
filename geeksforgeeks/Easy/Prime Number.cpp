c608cb882e0df8b5b7eaf2ab7ef8a772f26c8ba7
tanmaysingh108 <tanmaysingh1080@gmail.com>
2026-09-20 02:20:56 +0530

    Add Prime Number (Easy)

---
+++ geeksforgeeks/Easy/Prime Number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    bool isPrime(int n) {
+        if (n==1)
+            return(false);
+        bool final=true;
+        for(int i=2;i*i<=n;i++){
+            if (n%i==0){
+                final=false;
+            }
+        }
+        return(final);
+    }
+};
