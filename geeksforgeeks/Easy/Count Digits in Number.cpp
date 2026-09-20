b88e6372b21ee24f884bb4f6c561b5385af14cb3
Tanmay Singh <tanmaysingh1080@gmail.com>
2026-09-21 03:30:49 +0530

    Add Count Digits in Number (Easy)

--- geeksforgeeks/Easy/Count Digits in Number.cpp
+++ geeksforgeeks/Easy/Count Digits in Number.cpp
@@ -2,7 +2,7 @@ class Solution {
   public:
     int countDigits(int n) {
         for (int i=1;i<=10;i++){
-            if(((int)(n/pow(10,i)))==0){
+            if((n/pow(10,i))<1){
                 return(i);
             }
     }
