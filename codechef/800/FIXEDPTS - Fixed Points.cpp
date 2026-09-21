6f4b91d6c337b17535195f7aa034c4a511fa6ff8
Tanmay Singh <tanmaysingh1080@gmail.com>
2026-09-21 13:44:02 +0530

    Add Fixed Points (800)

---
+++ codechef/800/FIXEDPTS - Fixed Points.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	for(;t>0;t--){
+	    int n,k;
+	    cin>>n;
+	    cin>>k;
+	    if(n-k==1){
+	        cout<<"no\n";
+	    }
+	    else
+	        cout<<"yes\n";
+	}
+
+}
